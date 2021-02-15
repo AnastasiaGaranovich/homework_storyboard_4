a198f3bf53470996b4507d7f3332e6b6ff8089f2
NastyaGaranovich <nastyamadness@gmail.com>
2021-02-15 21:30:05 +0300

    Add sections label name and surname Add rows of sections person information

--- Homework_Storyboard_4/Homework_Storyboard_4/ListViewControllerWithSections.swift
+++ Homework_Storyboard_4/Homework_Storyboard_4/ListViewControllerWithSections.swift
@@ -3,17 +3,39 @@ import UIKit
 class ListViewControllerWithSections: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    let persons = AppData.person
     
 }
 
-//extension ListViewControllerWithSections: UITableViewDataSource, UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//    }
-//    
-//    
-//}
+extension ListViewControllerWithSections: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return persons.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            let person = persons[section]
+            return person.name + " " + person.surname
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerCell = UIView()
+        let label = UILabel(frame: CGRect(x: 20, y: 3, width: 300, height: 20))
+        let person = persons[section]
+        label.text = person.name + " " + person.surname
+        headerCell.backgroundColor = UIColor.lightGray
+        headerCell.addSubview(label)
+        return headerCell
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let person = persons[indexPath.section]
+        cell.textLabel?.text = indexPath.row == 0 ? person.email : person.phoneNumber
+        return cell
+    }
+}
